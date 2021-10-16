95d770ff039fe92b141617f63c5971a96db52181
anya P <kayablissfilms@gmail.com>
2021-10-16 11:56:37 -0400

    Added all files from AndroidBasics Project 3

--- app/src/main/java/com/ana/androidbasicsproject3/MainActivity.kt
+++ app/src/main/java/com/ana/androidbasicsproject3/MainActivity.kt
@@ -21,12 +21,15 @@ class MainActivity : AppCompatActivity() {
 
 
     val tasks = mutableListOf<Taskwritten>()
-    var positionEditted = -1
+    var positionEd = -1
     lateinit var adapter: TaskItemAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
         var resultLauncher =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
@@ -39,14 +42,14 @@ class MainActivity : AppCompatActivity() {
                     "ana",
                     "data passed from secondActivity" + data?.getStringExtra(INTENT_DATA_NAME)
                 )
-                //
+                //This is the data we get back
                 var userInputSecAct = data?.getStringExtra(INTENT_DATA_NAME)
                 //getStringExtra, allows us to get the data that was sent from one activity to another
-                var newTextSec = findViewById<TextView>(R.id.task).text.get(positionEditted)
+                var newTextSec = findViewById<TextView>(R.id.task).text.get(positionEd)
                 //tasks.add(userInputtedStringFromSecondActivity.toString())
 
                 if (userInputSecAct != null) {
-                    tasks[positionEditted] = Taskwritten(userInputSecAct)
+                    tasks[positionEd] = Taskwritten(userInputSecAct)
                     adapter.notifyDataSetChanged()
                 }
             }
@@ -57,24 +60,24 @@ class MainActivity : AppCompatActivity() {
 
         // When we create a new adapter, now we have to pass both a mutablelistof and an object, which is going to be an onItemClickListener
         // I am going to create a new object, which is going to be an onItemClickListener and I must implement this function called on item click
-        adapter = TaskItemAdapter(mutableListOf(), object : TaskItemAdapter.onItemClickListener {
+        adapter = TaskItemAdapter(tasks,
+            object : TaskItemAdapter.onItemClickListener {
             override fun onItemClick(index: Int) {
 
                 Log.i("ana", "Clicked on item in list: $index")
                 //positionEditted = index
 
+                positionEd = index
+
                 val intent = Intent(this@MainActivity, SecondActivity::class.java)
                 // var userInputTextMain = tasks[positionEditted].todoTask.toString()
-                intent.putExtra(INTENT_DATA_NAME, tasks[index])
+                intent.putExtra(INTENT_DATA_NAME, tasks[index].todoTask)
                 //startActivity(i)
                 resultLauncher.launch(intent)
             }
 
         })
 
-
-        // This will pass an empty list, to our adapter
-        //object
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)//arranges how things will layout
         recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
@@ -96,21 +99,6 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        //tasks.add(Taskwritten(newTask.toString()))
-        // adapter.notifyItemChanged(positionEditted)
-
-
-        /*fun deleteItems(index: Int){
-            tasks.removeAt(index)
-
-            // ** need to have it removed at the selected index based on checkbox
-        } */
-
-        // to fix
-        //1. set on clicklisener inside the adapter for the checkbox view
-        // 2. main activity :   were do a for loop that is going to check which checkboxes were checked.
-
-
         // Setting Delete button with onclickListener
 
         findViewById<FloatingActionButton>(R.id.fabDelete).setOnClickListener {
@@ -119,7 +107,6 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        // }
     }
 }
 
--- app/src/main/java/com/ana/androidbasicsproject3/SecondActivity.kt
+++ app/src/main/java/com/ana/androidbasicsproject3/SecondActivity.kt
@@ -15,25 +15,27 @@ class SecondActivity : AppCompatActivity() {
         val dataFromMainActivity = intent.getStringExtra(INTENT_DATA_NAME)
         Log.i("ana", "Data from main activity" + dataFromMainActivity)
         val secEditText = findViewById<EditText>(R.id.second_editTextView)
-        val secBtn = findViewById<Button>(R.id.submit_button)
-
-        val bundle: Bundle? = intent.extras
-        val input = bundle!!.getString(INTENT_DATA_NAME)
-
         secEditText.setText(dataFromMainActivity)
 
+        //val secBtn = findViewById<Button>(R.id.submit_button)
+
+       // val bundle: Bundle? = intent.extras
+       // val input = bundle!!.getString(INTENT_DATA_NAME)
+
+
+
         findViewById<Button>(R.id.submit_button).setOnClickListener {
-            val newUserInput = findViewById<EditText>(R.id.second_editTextView).text.toString()
             //Grab the text in the editText Field
+            val newUserInput = findViewById<EditText>(R.id.second_editTextView).text.toString()
+
             //Store the editText String in an intent
-            //close the activity
             val i = Intent()
             i.putExtra(INTENT_DATA_NAME, newUserInput)
 
             //Pass the intent back to MainActivity
             setResult(RESULT_OK, i)
 
-            //Exit/Finish this activity
+            //Exit/Finish this activity//close the activity
             finish()
 
 
